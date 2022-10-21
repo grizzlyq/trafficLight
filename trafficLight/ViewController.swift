2d0c9c3d0f50276a046944e1aacf93e7e7314c0a
grizzlyq <pechenika73@yandex.ru>
2022-10-21 17:18:36 +0300

    the final commit

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -10,13 +10,44 @@ import UIKit
 class ViewController: UIViewController {
 
   
+    @IBOutlet weak var redButton: UIView!
+    @IBOutlet weak var yellowButton: UIView!
+    @IBOutlet weak var greenButton: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redButton.layer.cornerRadius = 40
+        yellowButton.layer.cornerRadius = 40
+        greenButton.layer.cornerRadius = 40
+        
+        startButton.layer.cornerRadius = 20
+        redButton.alpha = 0.3
+        yellowButton.alpha = 0.3
+        greenButton.alpha = 0.3
     }
 
 
+    @IBAction func startButtonTapped() {
+        
+        if redButton.alpha <= 0.4 && yellowButton.alpha <= 0.4 && greenButton.alpha <= 0.4 {
+            startButton.setTitle("Next", for: .normal)
+            redButton.alpha = 1
+        } else if redButton.alpha == 1 && yellowButton.alpha <= 0.4 && greenButton.alpha <= 0.4 {
+            yellowButton.alpha = 1
+            redButton.alpha = 0.3
+        } else if redButton.alpha <= 0.4 && yellowButton.alpha == 1 && greenButton.alpha <= 0.4 {
+            greenButton.alpha = 1
+            yellowButton.alpha = 0.3
+        } else {
+            redButton.alpha = 1
+            yellowButton.alpha = 0.3
+            greenButton.alpha = 0.3
+        }
+    }
 }
 
